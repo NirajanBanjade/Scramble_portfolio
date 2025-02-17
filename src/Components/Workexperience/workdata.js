3ef1938344fe512b1b044e7fcbf8f2daa69ffb02
Nirajan Banjade <nirajanbanjade321@gmail.com>
2025-02-16 22:37:49 -0600

    work

--- src/Components/Workexperience/workdata.js
+++ src/Components/Workexperience/workdata.js
@@ -1,20 +1,21 @@
-
-export const WorkData =[
+export const WorkData = [
     {
-        title: "Research Assistant - (High Performance Computing Lab)",
-        date: "May 2024 - Present",
-        responsibilities:[
-            "Deployed a high-performance, GPU-accelerated data pipeline to achieve a remarkable 60x speedup in data scraping,generation, and parsing of optimized datasets from millions of PDFs.",
-            "Engineered a Python-based solution leveraging open-source Language Model (LLM) fine-tuning techniques on scientific PDFs, achieving a 60% improvement in summarization and query response time.",
-            "Developed a scalable, modular framework for continuous LLM fine-tuning, ensuring adaptability with seamless updates and achieving a 75% increase in model training throughput through optimized data pipelines.",
-        ]
+      title: "Research Assistant - (PerfML Lab)",
+      date: "May 2024 - Present",
+      responsibilities: [
+        "Engineered and automated a distributed GPU-accelerated data pipeline, utilizing PyTorch and parallel processing techniques, achieving a 60x speedup in extracting and converting text from PDFs to JSON format.",
+        "Implemented parallelized data processing for large-scale document scraping, enhancing efficiency and enabling faster query processing and LLM fine-tuning.",
+        "Fine-tuned pretrained LLMs using the generated dataset, developing a chatbot optimized for performance-related queries with improved response accuracy."
+      ]
     },
     {
-        title : "Teaching Assistant - (Physics Department)",
-        date: "Jan 2023 - May 2024",
-        responsibilities: ["Collaborated on engaging lectures and materials for Physics, Calculus-I, and Python, boosting course retention rates by 20 %.",
-        "Worked closely with faculty and students, providing insights and feedback to      enhance course content and delivery.",
+      title: "Teaching Assistant - (Physics Department)",
+      date: "Jan 2023 - May 2024",
+      responsibilities: [
+        "Collaborated on engaging lectures and materials for Physics, Calculus-I, and Python, boosting course retention rates by 20%.",
+        "Worked closely with faculty and students, providing insights and feedback to enhance course content and delivery.",
         "Engaged in collaborative problem-solving, contributing to a significant improvement in student performance by a remarkable 23% grade improvement."
-        ]
+      ]
     }
-]
+  ];
+  
